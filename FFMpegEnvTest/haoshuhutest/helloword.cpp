2885395082e8eb80f2cdca0ec1b6554e1a087244
haoshuhu <haoshuhu@bingo-yes.com.cn>
2020-08-13 16:57:00 +0800

    rtmp push success

---
+++ FFMpegEnvTest/haoshuhutest/helloword.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+}
+/*
+int main(int argc, char* argv[]) {
+	//printf("%s\n", avcodec_configuration());
+	printf("hello world");
+	return 0;
+}*/
